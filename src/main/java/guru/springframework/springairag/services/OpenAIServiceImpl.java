c0afd5d8e0059f2f2814f7d23c988da7c8dc1fb9
pabo0416 <pavlo.bohdanov@netcraker.com>
2024-05-09 15:53:46 +0300

    add controller and openai service

---
+++ src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
@@ -0,0 +1,26 @@
+package guru.springframework.springairag.services;
+
+import guru.springframework.springairag.model.Answer;
+import guru.springframework.springairag.model.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OpenAIServiceImpl implements OpenAIService {
+
+    final ChatClient chatClient;
+
+    @Override
+    public Answer getAnswer(Question question) {
+        PromptTemplate promptTemplate = new PromptTemplate(question.question());
+        Prompt prompt = promptTemplate.create();
+
+        ChatResponse response = chatClient.call(prompt);
+        return new Answer(response.getResult().getOutput().getContent());
+    }
+}
